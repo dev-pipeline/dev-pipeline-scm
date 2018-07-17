9875284e923cafa90f05eb8656ee1f38de3236c7
Stephen Newell <sjnewell@gmail.com>
2018-07-17 17:10:33 -0600

    Add make_simple_scm funciton; this removes the need for the common_wrapper argument

--- lib/devpipeline_scm/scm.py
+++ lib/devpipeline_scm/scm.py
@@ -7,10 +7,9 @@ import devpipeline_core.toolsupport
 import devpipeline_scm
 
 
-def _nothing_scm(current_target, common_wrapper):
+def _nothing_scm(current_target):
     # Unused variables
     del current_target
-    del common_wrapper
 
     class _NothingScm:
         def checkout(self, repo_dir):
@@ -27,7 +26,7 @@ def _nothing_scm(current_target, common_wrapper):
 _NOTHING_SCM = (_nothing_scm, "Do nothing.")
 
 
-def _make_scm(current_target, common_wrapper):
+def _make_scm(current_target):
     """
     Create an Scm for a component.
 
@@ -36,25 +35,7 @@ def _make_scm(current_target, common_wrapper):
     """
     return devpipeline_core.toolsupport.tool_builder(
         current_target["current_config"], "scm",
-        devpipeline_scm.SCMS, current_target, common_wrapper)
-
-
-class SimpleScm(devpipeline_core.toolsupport.SimpleTool):
-
-    """This class is a simple SCM tool."""
-
-    def __init__(self, real, current_target):
-        super().__init__(current_target, real)
-
-    def checkout(self, repo_dir):
-        """This function checks out source code."""
-        self._call_helper("Checking out", self.real.checkout,
-                          repo_dir)
-
-    def update(self, repo_dir):
-        """This funcion updates a checkout of source code."""
-        self._call_helper("Updating", self.real.update,
-                          repo_dir)
+        devpipeline_scm.SCMS, current_target)
 
 
 def scm_task(current_target):
@@ -64,7 +45,7 @@ def scm_task(current_target):
     Arguments
     target - The target to operate on.
     """
-    scm = _make_scm(current_target, lambda r: SimpleScm(r, current_target))
+    scm = _make_scm(current_target)
 
     src_dir = current_target["current_config"].get("dp.src_dir")
     scm.checkout(src_dir)
